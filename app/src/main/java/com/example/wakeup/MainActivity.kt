154c523c17094db6d088372ec6111dedbfad5d35
hawkast <lorenzo.guerrini001@studenti.uniparthenope.it>
2024-01-20 20:28:30 +0100

    change background image

--- app/src/main/java/com/example/wakeup/MainActivity.kt
+++ app/src/main/java/com/example/wakeup/MainActivity.kt
@@ -6,16 +6,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,16 +42,32 @@ fun MainScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Gray),
+                contentAlignment = Alignment.Center
 
-    )
+    ){
     Image(
 
-        painter = painterResource(id = R.drawable.imgsfondo),
+        painter = painterResource(id = R.drawable.img3),
 
         contentDescription = null,
-        modifier = Modifier.fillMaxSize(),
-        contentScale = ContentScale.Crop
+        modifier = Modifier
+            .size(500.dp)
+            .padding(bottom = 50.dp),
+
+
+        contentScale = ContentScale.Crop,
+
+
     )
+
+    Text(text = "FriendlyWakeUp by Lorenzo Guerrini",
+        modifier=Modifier
+            .align(Alignment.BottomCenter),
+           // .padding(bottom = 100.dp),
+    fontSize=10.sp,
+        color=Color.Black)
+   }
     if(isAggressive){
         Image(
 
